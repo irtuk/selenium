da2f73f2a5aca425aa8309f57b621bb28f8d789e
Edward Moore <remoore@btinternet.com>
2018-05-31 17:06:18 +0100

    Larne Dot work.

--- src/com/edwardmooreconsulting/poferries/PageObjectModel_Homepage.java
+++ src/com/edwardmooreconsulting/poferries/PageObjectModel_Homepage.java
@@ -1,6 +1,16 @@
 package com.edwardmooreconsulting.poferries;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class PageObjectModel_Homepage {
+	
+	public WebElement LarneDot (WebDriver Driver) {
+	WebElement myElement;
+	myElement = Driver.findElement(By.linkText("/larne-cairnryan"));
+	return myElement;
+	}
 	
 	
 	
---
+++ src/com/edwardmooreconsulting/poferries/testplan/ClickLarneDot.java
@@ -0,0 +1,14 @@
+package com.edwardmooreconsulting.poferries.testplan;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClickLarneDot {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
