b41393132beecfed02fc5ed7373566c256f3966a
Edward Moore <remoore@btinternet.com>
2018-06-11 13:15:55 +0100

    Started Database work. Added MySQL Jars to package and classpath

---
+++ src/apackage/Main.java
@@ -0,0 +1,11 @@
+package apackage;
+
+import apackage.MySQLAccess;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MySQLAccess dao = new MySQLAccess();
+        dao.readDataBase();
+    }
+
+}
